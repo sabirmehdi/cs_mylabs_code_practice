7b1044433d4a01ca6ac9a1edcbd44662e0bc8ca1
Sabir Mehdi <sabirmehdi528@gmail.com>
2025-11-29 13:24:45 +0500

    List create function

--- OOP_Labs/linkedlist.cpp
+++ OOP_Labs/linkedlist.cpp
@@ -1,27 +1,117 @@
+/*
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data= 10;
+    string next ="0x00d123DECF4";
+};
+
+int main(){
+    
+  //Baisc access to value through pointer
+    Node n1;
+    Node *p;
+    
+    p=&n1; // one way of getting address of the location, other one is with key word 'new'.
+    
+    cout<<"Print with simple Struct method: ";
+    cout<<n1.data<<endl;
+    
+    cout<<"Print with pointer 'p': ";
+    cout<<(*p).data<<endl;
+
+    cout<<"Print with (->) method: ";
+    cout<<p->data<<endl;
+    
+    cout<<"Notice the data is value is changed now: ";
+    (*p).data=20;
+    cout<<(*p).data<<endl;
+    
+    
+    // Creating new Node
+    
+    p= new Node;
+    
+    p->data= 30; // updating the data value of newly created node
+    
+    cout<<"This is address to new Node: "<<p<<endl; // prints the adress of new node
+    
+    cout<<"Data in New Node: "<<p->data<<endl; // prints the adress of new node
+    
+    
+    // Deleting an existing instance
+    
+    // using "delete" key word to mark the memory as free for OS.
+    
+    delete p;
+    
+    cout<<p;
+    
+    
+
+    
+}
+//*/
+
+
+// Creating list 
+
+#include <iostream>
+using namespace std;
+
 struct Node{
     int data;
     Node *next;
 };
+
+
+Node *create_node(Node *current ,int data){
+    
+    Node *n;
+    n= new Node;
+    
+    n->data=data;
+    n->next= NULL;
+    
+    current->next=n;
+    
+    return n;
+}
+
+
 int main(){
+   
    Node *head ; Node *current;
-   head=NULL;
    
    head=new Node;
    
-   head->data=10;
+   head->data=1;
    head->next=NULL;
    
-    cout<<(*head).data<<endl;
-    cout<<(*head).next;
-    
-    
-    current= new Node;
-    
-    head->next=current;
-    
-    current->data=20;
-    current->next=NULL;
+   
+   current= head;
+   
+   for (int i=2; i<=10;i++){
+       
+        current = create_node(current, i);
+
+   }
+   
+   current=head; // reetting current to head to display list in next loop
+   
+  while (current !=NULL){
+      
+      std::cout << "Node : " <<current->data<< std::endl;
+      
+      current= current->next;
+      
+  } 
+  
     
     return 0;
 }
+
 
